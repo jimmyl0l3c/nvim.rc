0b953366175cd9f0ea11486c6a74130ced4e0963
Filip Joska <filip@joska.dev>
2025-03-10 18:19:55 +0100

    Update LSP config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -28,10 +28,16 @@ return {
         local workspace_diagnostics = require("workspace-diagnostics")
         local capabilities = require('blink.cmp').get_lsp_capabilities()
 
+        local lspconfig = require("lspconfig")
+
+        -- Setup language servers installed manually
+        lspconfig.phpactor.setup({
+            capabilities = capabilities,
+            on_attach = workspace_diagnostics.populate_workspace_diagnostics,
+        })
+
+        -- Setup language servers installed with Mason
         require("mason-lspconfig").setup_handlers({
-            -- The first entry (without a key) will be the default handler
-            -- and will be called for each installed server that doesn't have
-            -- a dedicated handler.
             function(server_name) -- default handler (optional)
                 require("lspconfig")[server_name].setup({
                     capabilities = capabilities,
@@ -40,8 +46,7 @@ return {
             end,
 
             ["basedpyright"] = function()
-                local lspconfig = require("lspconfig")
-                lspconfig.basedpyright.setup({
+                require("lspconfig").basedpyright.setup({
                     capabilities = capabilities,
                     on_attach = workspace_diagnostics.populate_workspace_diagnostics,
                     settings = {
@@ -60,9 +65,8 @@ return {
             end,
 
             ["ruff"] = function()
-                local lspconfig = require("lspconfig")
                 local util = require('lspconfig.util')
-                lspconfig.ruff.setup({
+                require("lspconfig").ruff.setup({
                     capabilities = capabilities,
                     on_attach = workspace_diagnostics.populate_workspace_diagnostics,
                     single_file_support = false,
