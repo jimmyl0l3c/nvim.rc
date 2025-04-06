5401c8e245ad31e2a31fb1ff77c665bf8dac241f
Filip Joska <filip@joska.dev>
2025-04-06 15:39:15 +0200

    Update keymaps, enable diagnostic virtual_text by default

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -45,28 +45,42 @@ autocmd({ "BufWritePre" }, {
     command = [[%s/\s\+$//e]],
 })
 
+local function deprecate_keybind(new_bind, func)
+    vim.notify('Use "' .. new_bind .. '" instead', "warn")
+    func()
+end
+
 autocmd("LspAttach", {
     group = lsp_keymap_group,
     callback = function(e)
         local opts = { buffer = e.buf }
 
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+
+        vim.keymap.set("n", "gr", function() deprecate_keybind("grr", vim.lsp.buf.references) end, opts)
+        vim.keymap.set("n", "gi", function() deprecate_keybind("gri", vim.lsp.buf.implementation) end, opts)
+        vim.keymap.set("n", "<leader>vca", function() deprecate_keybind("gra", vim.lsp.buf.code_action) end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() deprecate_keybind("grn", vim.lsp.buf.rename) end, opts)
 
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set("n", "<leader>voi", function()
             vim.lsp.buf.code_action({
                 apply = true,
                 filter = function(x) return x.kind == "source.organizeImports.ruff" end,
             })
         end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts) -- TODO: remove in favor of <C-s>?
+
+        vim.keymap.set("n", "<leader>vt", function()
+            local current_config = vim.diagnostic.config()
+            vim.diagnostic.config({
+                virtual_lines = not (current_config or false).virtual_lines,
+                virtual_text = not (current_config or false).virtual_text,
+            })
+        end)
     end,
 })
+
+vim.diagnostic.config({ virtual_text = true })
