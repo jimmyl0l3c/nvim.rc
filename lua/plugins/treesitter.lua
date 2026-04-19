047bd1013e5b5536af042895a34f64eef9143d71
Filip Joska <filip@joska.dev>
2026-04-19 20:36:42 +0200

    update nvim-treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -16,40 +16,15 @@ return {
         "nvim-treesitter/nvim-treesitter-context",
         "phelipetls/vim-hugo",
     },
+    lazy = false,
     build = ":TSUpdate",
+    branch = "main",
     config = function()
-        require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all"
-            ensure_installed = {
-                "vimdoc",
-                "lua",
-                "bash",
-                "markdown",
-                "markdown_inline",
-            },
+        local ts = require("nvim-treesitter")
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+        ts.setup({})
 
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            indent = {
-                enable = true,
-            },
-
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = { "markdown" },
-            },
-        })
+        ts.install({ "vimdoc", "lua", "bash", "markdown", "markdown_inline" })
 
         -- gotmpl
         vim.treesitter.query.add_directive("inject-go-tmpl!", function(_, _, bufnr, _, metadata)
